575ffaf743abec98e5121de383bc1fca1cc6a8e4
Irena1703 <84846622+Irena1703@users.noreply.github.com>
2021-05-31 14:14:05 +0300

    lab1

---
+++ link_shortener/link_shortener/urls.py
@@ -0,0 +1,40 @@
+"""link_shortener URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ZipLink",
+      default_version='v1',
+      terms_of_service="https://www.google.com/policies/terms/",
+      description="Don't let the links limit you. Make your links support your brand. \
+      The new standard of shortening links.",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('admin/', admin.site.urls),
+    path('', include('zip_link.urls'))
+]
